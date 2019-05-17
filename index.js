da6f7d5068604588d534712f9ecaeb42949155f5
Alan Houston <alanhouston5000@gmail.com>
2019-05-17 16:32:43 -0500

    minor clean

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const io = require('socket.io')(http);
 
 let userNames = [];
 
-//serving html file
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('new user', function(data, callback){
-      callback(true)
-      socket.userName = data;
-      userNames.push(socket.userName);
+    callback(true)
+    socket.userName = data;
+    userNames.push(socket.userName);
   })
   
   socket.on('chat message', function(data){
-        io.emit('chat message', {msg: data, uname: socket.userName});
-    });
-    
+    io.emit('chat message', {msg: data, uname: socket.userName});
+  });
 });
 
 http.listen(3000, function(){
