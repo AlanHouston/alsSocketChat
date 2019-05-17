c43504c475f3cac3ca2354495a45c62773415873
Alan Houston <alanhouston5000@gmail.com>
2019-05-17 15:59:43 -0500

    add username functionality

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let userNames = [];
 
 //serving html file
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// listening on connection event for incoming sockets
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// });
-
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+  socket.on('new user', function(data, callback){
+      callback(true)
+      socket.userName = data;
+      userNames.push(socket.userName);
+  })
+  
+  socket.on('chat message', function(data){
+        io.emit('chat message', {msg: data, uname: socket.userName});
     });
+    
 });
 
-//server created, listening
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
